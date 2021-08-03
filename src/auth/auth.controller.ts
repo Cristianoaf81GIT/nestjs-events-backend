d0cc4c10066d9f8a197bc89065cf2812277b4dcb
cristiano alexandre de faria <cristianoaf81@gmail.com>
2021-08-03 16:00:45 -0300

    class51 generate token jwt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,23 @@
 import { Controller, Post, UseGuards, Request, Logger } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
+import { AuthService } from "./auth.service";
 
 
 @Controller('auth') 
 export class AuthController {
+    
     private readonly logger: Logger = new Logger(AuthController.name);
+
+    constructor(private readonly authService:AuthService) {}
+
+
     @Post('login')
     @UseGuards(AuthGuard('local'))
     async login(@Request() request) {
-        this.logger.debug('a new request arived')
+        this.logger.debug('a new  login request is arived')
         return {
             userId: request.user.id,
-            token: 'the token will go here'
+            token: this.authService.getTokenForUser(request.user)
         }
     }
 }
