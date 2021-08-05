fc656b79d79b674057d487af2db3ee72baa5c1d5
cristiano alexandre de faria <cristianoaf81@gmail.com>
2021-08-05 14:40:53 -0300

    class52 Strategy&guard - athenticating with jwt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Post, UseGuards, Request, Logger } from "@nestjs/common";
+import { 
+        Controller, 
+        Post, 
+        UseGuards, 
+        Request, 
+        Logger,
+        Get
+} from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 import { AuthService } from "./auth.service";
 
@@ -19,5 +26,11 @@ export class AuthController {
             userId: request.user.id,
             token: this.authService.getTokenForUser(request.user)
         }
+    }
+    
+    @Get('profile')
+    @UseGuards(AuthGuard('jwt'))
+    async getProfile(@Request() request) {
+      return request.user; 
     }
 }
